40f6cb5ffc382f5dc3fca23d50441268d7c531f6
Dawid Loranc <dawidloranc@gmail.com>
2024-05-02 22:24:22 +0200

    Add support for Reverse Polish Notation

--- first/app/src/main/java/com/craftinginterpreters/lox/Lox.java
+++ first/app/src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -55,6 +55,9 @@ public class Lox {
     if (hadError) return;
 
     System.out.println(new AstPrinter().print(expression));
+
+    // print Reverse Polish Notation
+    // System.out.println(new ReversePolishNotationAstPrinter().print(expression));
   }
 
   static void error(int line, String message) {
