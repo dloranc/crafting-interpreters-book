492b54c25f1fe00b9adb13989acfa72c6001a007
Dawid Loranc <dawidloranc@gmail.com>
2024-05-03 18:10:34 +0200

    Add comma operator

--- first/app/src/main/java/com/craftinginterpreters/lox/Parser.java
+++ first/app/src/main/java/com/craftinginterpreters/lox/Parser.java
@@ -22,7 +22,15 @@ class Parser {
   }
 
   private Expr expression() {
-    return equality();
+    Expr expr = equality();
+
+    while (match(TokenType.COMMA)) {
+      Token operator = previous();
+      Expr right = equality();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+
+    return expr;
   }
 
   private Expr equality() {
