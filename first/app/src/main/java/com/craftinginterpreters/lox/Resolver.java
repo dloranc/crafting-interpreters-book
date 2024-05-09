d7dea1dc2fe1d29286dca09deb8a0334c62782b2
Dawid Loranc <dawidloranc@gmail.com>
2024-05-09 21:31:28 +0200

    Handle invalid use of this (eg. outside a class body).

--- first/app/src/main/java/com/craftinginterpreters/lox/Resolver.java
+++ first/app/src/main/java/com/craftinginterpreters/lox/Resolver.java
@@ -16,6 +16,13 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     METHOD,
   }
 
+  private enum ClassType {
+    NONE,
+    CLASS
+  }
+
+  private ClassType currentClass = ClassType.NONE;
+
   Resolver(Interpreter interpreter) {
     this.interpreter = interpreter;
   }
@@ -30,6 +37,9 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   @Override
   public Void visitClassStmt(Stmt.Class stmt) {
+    ClassType enclosingClass = currentClass;
+    currentClass = ClassType.CLASS;
+
     declare(stmt.name);
     define(stmt.name);
 
@@ -44,6 +54,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     endScope();
 
+    currentClass = enclosingClass;
     return null;
   }
 
@@ -180,6 +191,10 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   @Override
   public Void visitThisExpr(Expr.This expr) {
+    if (currentClass == ClassType.NONE) {
+      Lox.error(expr.keyword, "Can't use 'this' outside of a class.");
+      return null;
+    }
     resolveLocal(expr, expr.keyword);
     return null;
   }
