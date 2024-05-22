0d6063f14fbe2d4fb17415303936a9a45eb7cdc5
Dawid Loranc <dawidloranc@gmail.com>
2024-05-22 21:57:30 +0200

    Free strings during concatenation

--- second/src/memory.c
+++ second/src/memory.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 
 #include "memory.h"
+#include "vm.h"
 
 void *reallocate(void *pointer, size_t oldSize, size_t newSize)
 {
@@ -18,4 +19,30 @@ void *reallocate(void *pointer, size_t oldSize, size_t newSize)
   }
 
   return result;
+}
+
+static void freeObject(Obj *object)
+{
+  switch (object->type)
+  {
+  case OBJ_STRING:
+  {
+    ObjString *string = (ObjString *)object;
+    FREE_ARRAY(char, string->chars, string->length + 1);
+    FREE(ObjString, object);
+    break;
+  }
+  }
+}
+
+void freeObjects()
+{
+  Obj *object = vm.objects;
+
+  while (object->next != NULL)
+  {
+    Obj *next = object->next;
+    freeObject(next);
+    object = next;
+  }
 }
