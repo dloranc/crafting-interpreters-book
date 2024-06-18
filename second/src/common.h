1146469fc0e67c86d2185cdda26c35fcab0d2c1c
Dawid Loranc <dawidloranc@gmail.com>
2024-06-19 00:21:39 +0200

    Add garbage collector

--- second/src/common.h
+++ second/src/common.h
@@ -1,8 +1,10 @@
 #ifndef clox_common_h
 #define clox_common_h
 
-// #define DEBUG_PRINT_CODE
-// #define DEBUG_TRACE_EXECUTION
+#define DEBUG_PRINT_CODE
+#define DEBUG_TRACE_EXECUTION
+#define DEBUG_STRESS_GC
+#define DEBUG_LOG_GC
 
 #define UINT8_COUNT (UINT8_MAX + 1)
 
